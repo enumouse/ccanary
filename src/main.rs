0f80b0ee579aa92748274663335a03d5ec025dcf
enumouse <90973461+enumouse@users.noreply.github.com>
2025-08-24 19:42:44 +0100

    Add ccanary_driver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
+/// Calls into ccanary_driver for heavy lifting
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    ccanary_driver::main(&args);
 }
